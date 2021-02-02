fc0332dc64d0dc17d6c491bf0eed2d6a24a43b9e
finally <fsocietywww@gmail.com>
2021-02-02 15:13:49 +0300

    major changes

--- django_training/urls.py
+++ django_training/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from core.views import index, topic_details
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index, name='index'),
+    path('topic/?P<pk>\\d+/', topic_details, name='topic_details'),
 ]
